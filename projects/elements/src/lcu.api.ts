810110c303ba7863b980fafb1c77865150a9bf95
Michael Gearhardt <michael.gearhardt@fathym.com>
2019-02-09 07:26:29 -0700

    Checkpoint

---
+++ projects/elements/src/lcu.api.ts
@@ -0,0 +1,1 @@
+export * from './lib/lcu-elements.module';
---
+++ projects/elements/src/lib/lcu-elements.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  exports: [],
+  entryComponents: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class LcuElementsModule { }
