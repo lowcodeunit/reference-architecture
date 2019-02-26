a74d9a534f8f786e2eb912ef14c9615c6ba782d8
Michael Gearhardt <michael.gearhardt@fathym.com>
2019-02-26 07:00:50 -0700

    Adjusted

--- projects/common/src/lib/api.api.ts
+++ projects/common/src/lib/api.api.ts
@@ -1,4 +1,5 @@
 export * from './api/daf/daf.service';
+export * from './api/lcu-service-settings';
 export * from './api/observable-context/observable-context.service';
 export * from './api/base-modeled-response';
 export * from './api/base-response';
