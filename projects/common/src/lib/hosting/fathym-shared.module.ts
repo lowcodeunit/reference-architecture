a72e0a8c4d4916872704f7baf34c2c1875f6b444
mcgear <michael.gearhardt@fathym.com>
2019-03-20 00:56:20 -0600

    Trying to get for AOT

--- projects/common/src/lib/hosting/fathym-shared.module.ts
+++ projects/common/src/lib/hosting/fathym-shared.module.ts
@@ -10,10 +10,6 @@ import { LCUServiceSettings } from '../api/lcu-service-settings';
 
 import 'hammerjs';
 
-export function winAny(): any {
-  return window;
-}
-
 @NgModule({
   imports: [CommonModule, FormsModule, HttpClientModule, RouterModule],
   exports: [CommonModule, FormsModule, HttpClientModule, RouterModule],
@@ -22,16 +18,16 @@ export function winAny(): any {
 })
 export class FathymSharedModule {
   //  Fields
-
   //  API Methods
   static forRoot(environment: { production: boolean }, apiRoot?: string): ModuleWithProviders {
+    const winAny = <any>window;
+
     const defaultSettings = <LCUServiceSettings>{
-      APIRoot:
-        apiRoot || (environment.production ? `` : winAny().LCU && winAny().LCU.APIRoot ? winAny().LCU.APIRoot : 'http://localhost:52235'),
+      APIRoot: apiRoot || (environment.production ? `` : winAny.LCU && winAny.LCU.APIRoot ? winAny.LCU.APIRoot : 'http://localhost:52235'),
       AppConfig: {
-        ID: winAny().LCU && winAny().LCU.Application && winAny().LCU.Application.ID ? winAny().LCU.Application.ID : 'test-app',
+        ID: winAny.LCU && winAny.LCU.Application && winAny.LCU.Application.ID ? winAny.LCU.Application.ID : 'test-app',
         EnterpriseAPIKey:
-          winAny().LCU && winAny().LCU.Application.EnterprisePrimaryAPIKey ? winAny().LCU.Application.EnterprisePrimaryAPIKey : 'test-app'
+          winAny.LCU && winAny.LCU.Application.EnterprisePrimaryAPIKey ? winAny.LCU.Application.EnterprisePrimaryAPIKey : 'test-app'
       }
     };
 
