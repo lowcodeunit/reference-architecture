eb97fe7592cc2a24a253add2876726bd0fc75eb1
xchanin <me@shannonbruns.com>
2019-05-01 09:37:50 -0600

    added angular materials module to fathym shared module

---
+++ projects/common/src/lib/modules/material.module.ts
@@ -0,0 +1,77 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+          MatButtonModule,
+          MatIconModule,
+          MatFormFieldModule,
+          MatInputModule,
+          MatCardModule,
+          MatDatepickerModule,
+          MatNativeDateModule,
+          MatCheckboxModule,
+          MatSidenavModule,
+          MatToolbarModule,
+          MatListModule,
+          MatTabsModule,
+          MatSelectModule,
+          MatProgressSpinnerModule,
+          MatDialogModule,
+          MatTableModule,
+          MatSortModule,
+          MatPaginatorModule,
+          MatProgressBarModule,
+          MatChipsModule
+         } from '@angular/material';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTabsModule,
+    MatSelectModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatDialogModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    FlexLayoutModule,
+    MatChipsModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTabsModule,
+    MatSelectModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatDialogModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    FlexLayoutModule,
+    MatChipsModule
+  ]
+})
+export class MaterialModule { }
