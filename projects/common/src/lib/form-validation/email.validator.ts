e3f145e598b438dde44b6e0b93914655d3dd2c60
xchanin <me@shannonbruns.com>
2019-05-06 07:15:30 -0600

    update email regex pattern to include domain

--- projects/common/src/lib/form-validation/email.validator.ts
+++ projects/common/src/lib/form-validation/email.validator.ts
@@ -6,8 +6,8 @@
 export class EmailValidator {
 
   /**
-   * Email regular expression pattern
+   * Email regular expression pattern, requires domain
    */
-  public static readonly EmailPattern: RegExp = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/gmi;
-  public static readonly EmailPattern2: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/gmi;
+  public static readonly EmailWithDomain: RegExp = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{3,4}$/gmi;
+
 }
