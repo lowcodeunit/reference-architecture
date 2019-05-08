ab0d3456aefef94fd8c24d2942ffd1c026658947
xchanin <me@shannonbruns.com>
2019-05-08 13:00:41 -0600

    update email patter validation string

--- projects/common/src/lib/form-validation/phone.validator.ts
+++ projects/common/src/lib/form-validation/phone.validator.ts
@@ -1,5 +1,5 @@
 import { AbstractControl, ValidatorFn } from '@angular/forms';
-import * as libphonenumber from 'google-libphonenumber';
+// import * as libphonenumber from 'google-libphonenumber';
 
 // @dynamic
 /**
@@ -7,46 +7,49 @@ import * as libphonenumber from 'google-libphonenumber';
  */
 export class PhoneValidator {
 
+  /**
+   * Commenting out for now
+   */
 // https://angular-templates.io/tutorials/about/angular-forms-and-validations
 
-  public static ValidCountryPhone = (countryControl: AbstractControl): ValidatorFn => {
+  // public static ValidCountryPhone = (countryControl: AbstractControl): ValidatorFn => {
 
-    let subscribe = false;
+  //   let subscribe = false;
 
-    return (control: AbstractControl): {[key: string]: boolean} => {
+  //   return (control: AbstractControl): {[key: string]: boolean} => {
 
-      if (!subscribe) {
-        subscribe = true;
-        countryControl.valueChanges.subscribe(() => {
-          control.updateValueAndValidity();
-        });
-      }
+  //     if (!subscribe) {
+  //       subscribe = true;
+  //       countryControl.valueChanges.subscribe(() => {
+  //         control.updateValueAndValidity();
+  //       });
+  //     }
 
-      if (control.value !== '') {
-        try {
+  //     if (control.value !== '') {
+  //       try {
 
-          const phoneUtil = libphonenumber.PhoneNumberUtil.getInstance();
-          const phoneNumber = '' + control.value + '';
-          const region = countryControl.value;
-          const pNumber = phoneUtil.parseAndKeepRawInput(phoneNumber, region.iso);
-          const isValidNumber = phoneUtil.isValidNumber(pNumber);
+  //         const phoneUtil = libphonenumber.PhoneNumberUtil.getInstance();
+  //         const phoneNumber = '' + control.value + '';
+  //         const region = countryControl.value;
+  //         const pNumber = phoneUtil.parseAndKeepRawInput(phoneNumber, region.iso);
+  //         const isValidNumber = phoneUtil.isValidNumber(pNumber);
 
-          if (isValidNumber) {
-            return undefined;
-          }
-        } catch (e) {
-          console.log(e);
-          return {
-            ValidCountryPhone: true
-          };
-        }
+  //         if (isValidNumber) {
+  //           return undefined;
+  //         }
+  //       } catch (e) {
+  //         console.log(e);
+  //         return {
+  //           ValidCountryPhone: true
+  //         };
+  //       }
 
-        return {
-          ValidCountryPhone: true
-        };
-      } else {
-        return undefined;
-      }
-    };
-  }
+  //       return {
+  //         ValidCountryPhone: true
+  //       };
+  //     } else {
+  //       return undefined;
+  //     }
+  //   };
+  // }
 }
