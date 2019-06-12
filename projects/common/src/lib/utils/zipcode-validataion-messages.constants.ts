c0ced903f53aa96cfbc22f4fdcb32f2c42783b8a
xchanin <me@shannonbruns.com>
2019-06-12 10:14:21 -0600

    add zipcode error message

---
+++ projects/common/src/lib/utils/zipcode-validataion-messages.constants.ts
@@ -0,0 +1,21 @@
+// @dynamic
+/**
+ * @dynamic need this because there are static memebers
+ *
+ * Email constants
+ */
+
+export class ZipcodeValidationMessagesConstants {
+
+    /**
+     * Constant for email pattern
+     */
+    public static readonly PATTERN: string = 'Please enter a valid zipcode';
+  
+    /**
+     * Constant for email required
+     */
+    public static readonly REQUIRED: string = 'Email is required';
+  
+  }
+  
