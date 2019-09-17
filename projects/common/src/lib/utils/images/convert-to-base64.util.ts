edcddb1471600ea4675b4801dbe46b5a25c5a151
powhound4 <george.hatch@fathym.com>
2019-09-17 14:24:14 -0600

    replace function

--- projects/common/src/lib/utils/images/convert-to-base64.util.ts
+++ projects/common/src/lib/utils/images/convert-to-base64.util.ts
@@ -7,19 +7,24 @@ import { Base64Model } from '../../models/base64.model';
  */
 export class ConvertToBase64Util{
 
-    
+    /**
+     * 
+     * @param event either type File or Blob 
+     * 
+     * Converts to Base64 and returns Object{base64 string, original file/blob passed in}
+     */
 public static GetBase64(event): Base64Model {
     let file = event;
     let me = this;
     let reader = new FileReader();
     let base64Object: Base64Model;
     reader.readAsDataURL(file);
-    reader.onload = () => {
+    reader.onload = function ()  {
       //console.log(reader.result);
       base64Object = new Base64Model(reader.result.toString(), file);
     //   me.buildImageMessage(reader.result.toString(), file);
     };
-    reader.onerror = (error) => {
+    reader.onerror = function (error) {
       console.log('Error: ', error);
       return;
     };
