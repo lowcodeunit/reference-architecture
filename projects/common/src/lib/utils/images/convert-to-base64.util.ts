eec92bb8f91af97d5d97e78f9b32c0b2d2f7e313
xchanin <me@shannonbruns.com>
2019-09-17 18:40:24 -0600

    return observable

--- projects/common/src/lib/utils/images/convert-to-base64.util.ts
+++ projects/common/src/lib/utils/images/convert-to-base64.util.ts
@@ -1,42 +1,32 @@
 import { Base64Model } from '../../models/base64.model';
-import { Observable, Subscriber } from 'rxjs';
+import { Observable, Subscriber, Subject } from 'rxjs';
 
 // @dynamic
 
 /**
  * @dynamic is used because this class contains static properties
  */
-export class ConvertToBase64Util{
+export class ConvertToBase64Util {
 
     /**
-     * 
-     * @param event either type File or Blob 
-     * 
+     *
+     * @param event either type File or Blob
+     *
      * Converts to Base64 and returns Object{base64 string, original file/blob passed in}
      */
-public static GetBase64(event): Observable<Base64Model> {
-    const reader = new FileReader();
-    let base64Object: Base64Model;
+    public static GetBase64(event: File): Observable<Base64Model> {
+      const reader = new FileReader();
+      const base64Observable = new Subject<Base64Model>();
 
-    return Observable.create((observer: Subscriber<any>): void => {
-      reader.onload = ((ev: Event): void => {
-        console.log('reader', reader);
-        console.log('ev', ev);
-        base64Object = new Base64Model(reader.result.toString(), event);
-      });
+      reader.onload = () => {
+        base64Observable.next(new Base64Model(reader.result.toString(), event));
+      };
 
-      observer.next(this);
-      observer.complete();
-    });
+      reader.readAsDataURL(event);
+      reader.onerror = (error) => {
+        console.log('Error: ', error);
+      };
 
-    reader.readAsDataURL(event);
-    // reader.onload = (val) => {
-    //   console.log('val', val);
-    //   // return () => new Base64Model(reader.result.toString(), file);
-    // };
-
-    reader.onerror = (error) => {
-      console.log('Error: ', error);
-    };
- }
+      return base64Observable;
+   }
 }
