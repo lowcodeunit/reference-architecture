0a6b0c73181663893da112c9b73ccc3a04c9d938
xchanin <me@shannonbruns.com>
2019-09-20 16:14:53 -0600

    some cleanup

--- projects/common/src/lib/utils/images/convert-to-base64.util.ts
+++ projects/common/src/lib/utils/images/convert-to-base64.util.ts
@@ -15,23 +15,23 @@ export class ConvertToBase64Util {
      *
      * Converts to Base64 and returns an observable of Base64Model
      */
-    public static GetBase64(file: Array<File>): Observable<Array<Base64Model>> {
+    public static GetBase64(files: Array<File>): Observable<Array<Base64Model>> {
       const base64Observable = new Subject<Array<Base64Model>>();
       const baseArray: Array<Base64Model> = [];
+      let file: File;
 
-      for (const itm of file) {
+      for (file of files) {
         const reader = new FileReader();
 
         reader.onload = () => {
-          baseArray.push(new Base64Model(reader.result.toString(), itm));
+          baseArray.push(new Base64Model(reader.result.toString(), file));
 
-          if (baseArray.length === file.length) {
+          if (baseArray.length === files.length) {
             base64Observable.next(baseArray);
           }
-          // base64Observable.next(new Base64Model(reader.result.toString(), itm));
         };
 
-        reader.readAsDataURL(itm['file'].rawFile);
+        reader.readAsDataURL(file['file'].rawFile);
         reader.onerror = (error) => {
           console.error('Error: ', error);
         };
