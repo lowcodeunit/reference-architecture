bf1062491102eec116e6fe77ff1437a2aebc9e09
xchanin <me@shannonbruns.com>
2019-10-16 12:24:52 -0600

    export sanitize pipe

--- projects/common/src/lib/utils.api.ts
+++ projects/common/src/lib/utils.api.ts
@@ -3,3 +3,4 @@ export * from './utils/conversion/temperature.conversion';
 export * from './utils/images/convert-to-base64.util';
 export * from './utils/theming/toggle-theme.utils';
 export * from './utils/constants/data-pipe.constants';
+export * from './utils/pipes/sanitze-html.pipe';
