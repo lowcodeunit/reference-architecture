7ec36861c4a49f936f3d3b3a9c6452b4b94a4b80
xchanin <me@shannonbruns.com>
2019-10-16 12:18:21 -0600

    sanitize pipe

--- projects/common/src/lib/utils/pipes/sanitze-html.pipe.ts
+++ projects/common/src/lib/utils/pipes/sanitze-html.pipe.ts
@@ -1,7 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 
-@Pipe({ name: 'sanitizeHtml'})
+/**
+ * Pipe used to sanitze html
+ */
+@Pipe({
+    name: 'sanitizeHtml'
+})
 export class SafeHtmlPipe implements PipeTransform  {
   constructor(private sanitized: DomSanitizer) {}
   transform(value) {
