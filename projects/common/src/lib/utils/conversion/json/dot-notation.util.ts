9cf62bb888c57e4457de05de7d7ce7e440807a48
xchanin <me@shannonbruns.com>
2019-11-14 07:27:04 -0700

    added getvalue to dot notation

--- projects/common/src/lib/utils/conversion/json/dot-notation.util.ts
+++ projects/common/src/lib/utils/conversion/json/dot-notation.util.ts
@@ -4,7 +4,7 @@
  * @dynamic is used because this class contains static properties
  */
 
-export class DotNotationChangeUtil {
+export class DotNotationUtil {
 
     /**
      * Using dot notation, iterate the object and change the key value
@@ -15,12 +15,26 @@ export class DotNotationChangeUtil {
      *
      * @param newVal Changed value
      */
-    public static ValueChange(json: JSON, dotPath: string, newVal: string): void {
+    public static ValueChange(json: JSON | object, dotPath: string, newVal: string): void {
         return dotPath.split('.').reduce( (prev, curr, idx, arr) => {
           if ( idx === (arr.length - 1) && prev ) {
               prev[curr] = newVal;
           }
           return prev ? prev[curr] : null;
       }, json);
+    }
+
+    /**
+     * Drill down to find nested objects
+     *
+     * @param json object to test
+     *
+     * @param pathArr array of names used to drill into objects
+     */
+    public static GetValue(json: JSON | object, pathArr: Array<string>): any {
+        const val = pathArr.reduce((obj, key) =>
+            (obj && obj[key] !== 'undefined') ? obj[key] : undefined, json);
+
+        return val;
     }
 }
