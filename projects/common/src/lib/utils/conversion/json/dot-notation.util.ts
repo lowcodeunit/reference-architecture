bf24acb163df413e0acc22891bc9b1edf63a3e95
xchanin <me@shannonbruns.com>
2019-11-21 13:08:34 -0700

    add setkeyvalue

--- projects/common/src/lib/utils/conversion/json/dot-notation.util.ts
+++ projects/common/src/lib/utils/conversion/json/dot-notation.util.ts
@@ -1,3 +1,4 @@
+import { JSONSchema } from './../../../json-schema';
 // @dynamic
 
 /**
@@ -46,13 +47,71 @@ export class DotNotationUtil {
      *
      * @param pathArr array of names used to drill into objects
      */
-    public static GetValue(obj: JSON | object, propertyPath: Array<string>): string {
+    public static GetValue(obj: JSON | object, propertyPath: string): string {
         /**
+         * split propertyPath string into an array of strings and iterate each itm with reduce
+         *
          * @param acc(accumulator) returned object to iterate
          *
          * @param curr(current value) current element being processed in the array
+         *
+         * @param idx index position of the current element being processed in the array
+         *
+         * @param arr array we created with the split function above
+         *
          */
-        return propertyPath.reduce((acc, curr) =>
-            (obj && obj[curr] !== 'undefined') ? obj[curr] : undefined, obj);
+      return propertyPath.split('.').reduce( (acc, curr, idx, arr) => {
+        if ( idx === (arr.length - 1) && acc ) {
+          return acc[curr];
+        }
+        // return a new accumulator to the reduce callback(starts the loop with the next curr value)
+        return acc ? acc[curr] : null;
+
+      // inital value to use as the first argument(this is the item to start the iteration with)
+    }, obj);
+  }
+  public static SetKeyValue(json: JSON | JSONSchema, propertyPath: string, oldKey: string, newKey: string, objToChange: string): any {
+
+    // for top level properties
+    if (propertyPath.split('.').length === 1) {
+      return Object.keys(json).reduce( (acc, curr, idx, arr) =>
+        curr === oldKey ? ({ ...acc, [newKey]: json[oldKey] }) : ({...acc, [curr]: json[curr]}), {} );
     }
+
+    // for nested properties
+    if (propertyPath.split('.').length > 1) {
+      return propertyPath.split('.').reduce( (acc, curr, idx, arr) => {
+          if ( idx === (arr.length - 1) && acc ) {
+
+            const idxPos: number = arr.indexOf(objToChange);
+            const idxPosArr: Array<string> = [ ...arr.splice(0, idxPos + 1) ];
+
+            DotNotationUtil.SetValue(json, idxPosArr.join('.'), this.RenameKeys({[oldKey]: newKey}, acc));
+
+            return json;
+            // return {
+            //   ...schema, [objToChange]: this.renameKeys({[oldKey]: newKey}, acc)
+            // };
+          }
+          return acc ? acc[curr] : null; // if acc, then start additional iterations with acc[curr]
+      }, json); // first item to start the loop
+    }
+  }
+
+  /**
+   * Rename property keys
+   */
+  public static RenameKeys(keysMap, obj): any {
+    return Object
+            .keys(obj)
+            .reduce((acc, key) => {
+              const renamedObject = {
+                [keysMap[key] || key]: obj[key]
+              };
+              return {
+                ...acc,
+                ...renamedObject
+              };
+            }, {});
+  }
 }
