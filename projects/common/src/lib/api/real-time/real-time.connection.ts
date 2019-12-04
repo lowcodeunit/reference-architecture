05bacb8f7c80a3f78855e0949fa63a5522cfaea4
mcgear <michael.gearhardt@gmail.com>
2019-12-04 01:09:05 -0700

    More lightweight Hub connection

---
+++ projects/common/src/lib/api/real-time/real-time.connection.ts
@@ -0,0 +1,167 @@
+import * as signalR from '@aspnet/signalr';
+import { NgZone, Output, EventEmitter } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
+import { LCUServiceSettings } from '../lcu-service-settings';
+import {
+  Observable,
+  BehaviorSubject,
+  ReplaySubject,
+  Observer,
+  Subject
+} from 'rxjs';
+
+export class RealTimeConnection {
+  //  Fields
+  protected connectionAttempts: number;
+
+  protected hub: signalR.HubConnection;
+
+  protected rtUrl: string;
+
+  //  Properties
+  public ConnectionError: EventEmitter<any>;
+
+  public MaxConnectionRetryAttempts: number;
+
+  public ReconnectionAttempt: EventEmitter<number>;
+
+  public Started: EventEmitter<signalR.HubConnection>;
+
+  //  Constructors
+  constructor(rtUrl: string, maxConnectionRetryAttempts: number = 10) {
+    this.connectionAttempts = 0;
+
+    this.rtUrl = rtUrl;
+
+    this.ConnectionError = new EventEmitter<any>();
+
+    this.ReconnectionAttempt = new EventEmitter<number>();
+
+    this.MaxConnectionRetryAttempts = maxConnectionRetryAttempts;
+
+    this.Started = new EventEmitter<signalR.HubConnection>();
+  }
+
+  //  API Methods
+  public Start(
+    transport: signalR.HttpTransportType = signalR.HttpTransportType.WebSockets
+  ) {
+    this.buildHub(transport).then((hub: signalR.HubConnection) => {
+      this.hub = hub;
+
+      this.hub.serverTimeoutInMilliseconds = 600000;
+
+      this.hub.onclose(err => {
+        console.log('onclose: ' + err);
+
+        this.retryConnection();
+      });
+
+      try {
+        this.hub
+          .start()
+          .then(() => {
+            this.connectionAttempts = 0;
+
+            console.log(`Connection started`);
+
+            this.Started.emit(this.hub);
+          })
+          .catch(err => {
+            console.log('Error while starting connection: ' + err);
+
+            this.ConnectionError.emit(err);
+
+            this.retryConnection();
+          });
+      } catch (err) {
+        console.log('Error while starting connection: ' + err);
+
+        this.retryConnection();
+      }
+    });
+  }
+
+  public RegisterHandler(methodName: string) {
+    return Observable.create(obs => {
+      if (this.hub) {
+        try {
+          this.hub.on(methodName, req => {
+            obs.next(req);
+          });
+        } catch (err) {
+          console.log(`Error while handling ${methodName}: ` + err);
+
+          obs.error(err);
+        }
+      } else {
+        obs.error(
+          'The hub must be started and configured before registering a handler.'
+        );
+      }
+    });
+  }
+
+  public Invoke(methodName: string, ...args: any[]) {
+    return Observable.create(obs => {
+      if (this.hub) {
+        try {
+          this.hub
+            .invoke(methodName, ...args)
+            .then(res => {
+              obs.next(res);
+            })
+            .catch(e => {
+              obs.error(e);
+            });
+        } catch (err) {
+          console.log(`Error while invoking ${methodName}: ` + err);
+
+          obs.error(err);
+        }
+      } else {
+        obs.error('The hub must be started and configured before invoking.');
+      }
+    });
+  }
+
+  //  Helpers
+  protected async buildHub(transport: signalR.HttpTransportType) {
+    return new signalR.HubConnectionBuilder()
+      .configureLogging(signalR.LogLevel.Information)
+      .withUrl(this.rtUrl, {
+        transport
+      })
+      .build();
+  }
+
+  protected stop(): Promise<void> {
+    return this.hub.stop();
+  }
+
+  /**
+   * Retry connection
+   */
+  protected retryConnection(): void {
+    if (this.connectionAttempts < this.MaxConnectionRetryAttempts) {
+      console.log(`Retrying connection attempt ${this.connectionAttempts}`);
+
+      this.connectionAttempts += 1;
+
+      this.reconnect();
+    } else if (this.connectionAttempts >= this.MaxConnectionRetryAttempts) {
+      this.stop().then();
+
+      this.ConnectionError.emit('The maximum number of connection retries has been met.')
+    }
+  }
+
+  /**
+   * Attempt to reconnect
+   */
+  protected reconnect(): void {
+    this.ReconnectionAttempt.emit(this.connectionAttempts);
+
+    this.Start();
+  }
+}
