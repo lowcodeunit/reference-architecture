1f9cb9670e6d902457e0284db8e69ca9b7de3c36
xchanin <me@shannonbruns.com>
2019-12-05 06:30:38 -0700

    add services

---
+++ projects/common/src/lib/services.api.ts
@@ -0,0 +1,1 @@
+export * from './services/theming/theme-color-picker.utils';
---
+++ projects/common/src/lib/services/theming/theme-color-picker.utils.ts
@@ -0,0 +1,44 @@
+// https://ajonp.com/courses/angularmaterial/angular-material-theming/
+
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class ThemeColorPickerService {
+
+    protected colorClass: BehaviorSubject<string>;
+    protected initialClass: string;
+
+    constructor(protected overlayContainer: OverlayContainer) {
+
+        this.initialClass = 'fathym-contrast-theme';
+        this.colorClass = new BehaviorSubject(this.initialClass);
+
+        const storageClass = localStorage.getItem('theme-picker');
+        console.log('color picker service', storageClass);
+        if (storageClass) {
+            overlayContainer.getContainerElement().classList.add(storageClass);
+            this.colorClass.next(storageClass);
+        } else {
+            overlayContainer.getContainerElement().classList.add(this.initialClass);
+        }
+    }
+
+    public GetColorClass(): BehaviorSubject<string> {
+        return this.colorClass;
+    }
+
+    public SetColorClass(className: string) {
+        this.overlayContainer.getContainerElement().classList.forEach(css => {
+            this.overlayContainer.getContainerElement().classList.remove(css);
+        });
+
+        this.overlayContainer.getContainerElement().classList.add(className);
+        this.colorClass.next(className);
+        localStorage.setItem('theme-picker', className);
+    }
+}
