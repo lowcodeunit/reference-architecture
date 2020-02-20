3623a3682a67038fde2a16a4890ef2c72846bb52
xchanin <me@shannonbruns.com>
2020-02-20 06:54:59 -0700

    remove hammerjs from shared module

--- projects/common/src/lib/hosting/fathym-shared.module.ts
+++ projects/common/src/lib/hosting/fathym-shared.module.ts
@@ -8,8 +8,6 @@ import { LCUInterceptor } from '../api/daf/lcu.interceptor';
 import { RealTimeService } from '../api/real-time/real-time.service';
 import { LCUServiceSettings } from '../api/lcu-service-settings';
 
-import 'hammerjs';
-
 export const winAny = <any>window;
 
 @NgModule({
