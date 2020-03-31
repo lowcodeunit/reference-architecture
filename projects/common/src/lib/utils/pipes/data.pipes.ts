bb4ddc9838cf1b4dff383b2bd5ea85fe248c3c97
powhound4 <george.hatch@fathym.com>
2020-03-31 10:44:07 -0600

    data pipes debug

--- projects/common/src/lib/utils/pipes/data.pipes.ts
+++ projects/common/src/lib/utils/pipes/data.pipes.ts
@@ -21,8 +21,10 @@ export class DataPipes implements PipeTransform {
      * Return date
      */
     if (args.toLowerCase() === DataPipeConstants.PIPE_DATE) {
+      console.log("ARGS: ", args)
       const pipe = new DatePipe('en-US');
       const transformed = pipe.transform(value, DataPipeConstants.DATE_FMT);
+      console.log("Transformed: ", transformed);
       return transformed;
     }
 
@@ -30,8 +32,9 @@ export class DataPipes implements PipeTransform {
      * Return date M/d/YYYY
      */
     if (args.toLowerCase() === DataPipeConstants.PIPE_SHORTDATE) {
+      console.log("ARGS: ", args)
       const pipe = new DatePipe('en-US');
-      const transformed = pipe.transform(value, DataPipeConstants.DATE_SHORTDATE);
+      const transformed = pipe.transform(value, 'shortDate');
       return transformed;
     }
 
@@ -39,8 +42,9 @@ export class DataPipes implements PipeTransform {
      * Return date MMM d, y
      */
     if (args.toLowerCase() === DataPipeConstants.PIPE_MEDIUMDATE) {
+      console.log("ARGS: ", args)
       const pipe = new DatePipe('en-US');
-      const transformed = pipe.transform(value, DataPipeConstants.DATE_MEDIUMDATE);
+      const transformed = pipe.transform(value, 'MMM d, y');
       return transformed;
     }
 
