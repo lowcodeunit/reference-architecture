7ae33dacf9401e2217585072970a32d3ff9a1bc6
xchanin <me@shannonbruns.com>
2020-05-20 07:53:34 -0600

    change color directive selector

--- projects/common/src/lib/directives/themes/theme-color.directive.ts
+++ projects/common/src/lib/directives/themes/theme-color.directive.ts
@@ -15,7 +15,7 @@ import { ThemePalette } from '@angular/material/core';
 // type ColorClasses = 'primary-500' | 'accent-500' | 'warn-500';
 
 @Directive({
-  selector: '[lcuThemeColor]'
+  selector: '[color]'
 })
 
 export class ThemeColorDirective {
@@ -23,7 +23,7 @@ export class ThemeColorDirective {
 /**
  * Set the current theme class to the element
  */
-  @Input() set lcuThemeColor(val: string) {
+  @Input() set color(val: string) {
 
     // this.renderer.removeClass(this.element.nativeElement, `${val}`);
     this.renderer.addClass(this.element.nativeElement, `${val}`);
