2bd1c602d05f4de160572c5c60a9e37fffafb9d0
Bobby Wise <robert.wise@fathym.com>
2020-06-04 15:12:07 -0600

    Made constants values lowercase.

--- projects/common/src/lib/utils/constants/string-pipe.constants.ts
+++ projects/common/src/lib/utils/constants/string-pipe.constants.ts
@@ -5,8 +5,8 @@
  * String pipe constants
  */
 export class StringPipeConstants {
-  public static readonly PIPE_CAMEL_CASE = 'camelCase';
-  public static readonly PIPE_KEBAB_CASE = 'kebabCase';
-  public static readonly PIPE_PASCAL_CASE = 'pascalCase';
-  public static readonly PIPE_SNAKE_CASE = 'snakeCase';
+  public static readonly PIPE_CAMEL_CASE = 'camelcase';
+  public static readonly PIPE_KEBAB_CASE = 'kebabcase';
+  public static readonly PIPE_PASCAL_CASE = 'pascalcase';
+  public static readonly PIPE_SNAKE_CASE = 'snakecase';
 }
