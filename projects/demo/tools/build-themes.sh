402ad49b9fa6a77d18b4590ac7bb790c8e0089f2
xchanin <me@shannonbruns.com>
2020-06-24 09:19:15 -0600

    create prebuilt themes and copy to common

---
+++ projects/demo/tools/build-themes.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+DEST_PATH=src/assets
+INPUT_PATH=$DEST_PATH/custom-themes/
+
+
+echo Building custom theme scss files.
+
+# Get the files
+FILES=$(find projects/demo/src/assets/custom-themes -name "*.scss")
+# echo $FILES
+
+for FILE in $FILES
+do
+  # FILENAME=ivy  
+  FILENAME=${FILE#$INPUT_PATH}
+  echo FILENAME: $FILENAME  
+  echo INPUTPATH: $INPUT_PATH
+
+  # Remove .scss extension
+  BASENAME=${FILENAME%.scss}
+  echo BASENAME: $BASENAME
+  echo FILE: $FILE
+  echo DEST: $DEST_PATH
+  echo DESTPATH AND BASENAME: $DEST_PATH/$BASENAME.css
+  echo NPM: $(npm bin)/node-sass
+  $(npm bin)/node-sass $FILE > $BASENAME.css
+done
+
+echo Finished building CSS.
