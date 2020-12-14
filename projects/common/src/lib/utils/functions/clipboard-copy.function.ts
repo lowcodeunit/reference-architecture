c1a1cc3a6c40af0bf3e3d76e35dce25f81d1a7ac
powhound4 <george.hatch@fathym.com>
2020-12-14 12:15:52 -0700

    ab#8979 add copy function

---
+++ projects/common/src/lib/utils/functions/clipboard-copy.function.ts
@@ -0,0 +1,21 @@
+// @dynamic
+
+/**
+ * @dynamic is used because this class contains static properties
+ */
+
+export class ClipboardCopyFunction {
+    /**
+         * Copies the string content to the clipboard for the user to paste
+         * @param copyContent the content to copy to the clipboard
+         */
+        public  static ClipboardCopy(copyContent: string): void {
+            const el = document.createElement('textarea');
+            el.value = copyContent;
+            document.body.appendChild(el);
+            el.select();
+            document.execCommand('copy');
+            document.body.removeChild(el);
+        }
+    
+    }
