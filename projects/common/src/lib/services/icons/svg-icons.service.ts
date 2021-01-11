05ac0c67aca64ebc49c4aeca603de39a89abc5c6
xchanin <shannonwbruns@gmail.com>
2021-01-11 08:58:27 -0700

    AB#8783 add svg icon service and model

---
+++ projects/common/src/lib/services/icons/svg-icons.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { SVGIconsModel } from '../../models/icons/svg-icons.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class SvgIconsService {
+
+  /**
+   * List of icons
+   */
+  protected icons: Array<SVGIconsModel>;
+
+  constructor(
+    protected domSanitizer: DomSanitizer,
+    protected matIconRegistry: MatIconRegistry) {
+   }
+
+   /**
+    *
+    * @param icons List of svg icons
+    * @param basePath base path to svg icons (/assets/svgs/etc.)
+    */
+  public SetIcons(icons: Array<SVGIconsModel>, basePath: string): void {
+
+    this.icons = icons;
+
+    this.icons.forEach((icon: SVGIconsModel) => {
+      this.matIconRegistry.addSvgIcon(
+        icon.Name,
+        this.setPath(`${basePath}${icon.IconPath}`));
+    });
+  }
+
+  /**
+   * Return list of icons
+   */
+  public GetIcons(): Array<SVGIconsModel> {
+    return this.icons;
+  }
+
+  /**
+   * Register icon path
+   *
+   * @param url path to svg
+   */
+  protected setPath(url: string): SafeResourceUrl  {
+    return this.domSanitizer.bypassSecurityTrustResourceUrl(url);
+    }
+}
