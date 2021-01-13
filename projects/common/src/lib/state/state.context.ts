96b684fcb67c3d5388511e971cce74fcba89703a
powhound4 <george.hatch@fathym.com>
2021-01-13 10:32:05 -0700

    ab#8980 change return type of executeAction

--- projects/common/src/lib/state/state.context.ts
+++ projects/common/src/lib/state/state.context.ts
@@ -167,7 +167,7 @@ export abstract class StateContext<T> extends ObservableContextService<T> {
  
     const stateName = this.loadStateName();
  
-    return new Promise<string>((resolve, reject) => {
+    return new Promise<Observable<Object>>((resolve, reject) => {
       return this.rt
         .InvokeAction(action.Type, this.loadHeaders(), {
           ...action,
