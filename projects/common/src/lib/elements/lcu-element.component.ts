4b4e12f8839cd8e19340ea12beb9b75723d78b70
mcgear <michael.gearhardt@gmail.com>
2021-02-15 17:05:34 -0700

    Change to proper casing now that its supported

--- projects/common/src/lib/elements/lcu-element.component.ts
+++ projects/common/src/lib/elements/lcu-element.component.ts
@@ -5,9 +5,8 @@ export abstract class LcuElementComponent<T> implements OnInit {
   //  Fields
 
   //  Properties
-  //  TODO: WOuld be ideal if this was caps with renamed input to lower...  But Angular Elements doesn't honor the Input/Output rename
-  @Input()
-  public context: T;
+  @Input('context')
+  public Context: T;
 
   //  Constructors
   constructor(protected injector: Injector) {
@@ -19,6 +18,6 @@ export abstract class LcuElementComponent<T> implements OnInit {
   //  API Methods
   @Input()
   public SetContext(ctxt: T) {
-    this.context = ctxt;
+    this.Context = ctxt;
   }
 }
